bca2b8fcbff0c3e7f9b0a62b9dfdfca3c05cb4db
Damien Mayance <apps@mayance.io>
2021-05-13 23:33:03 +0200

    :sparkles: First table downloader!

--- Assets/Notion-Unity/NotionDownloader.cs
+++ Assets/Notion-Unity/NotionDownloader.cs
@@ -26,37 +26,14 @@ namespace NotionUnity
 
             for (int i = 0; i < r.Lines.Length; i++)
             {
-                Debug.Log("--------");
-                var s = r.Value(i, "Person");
-                Debug.Log(i + " person " + s);
-                
-                s = r.Value(i, "Name");
-                Debug.Log(i + " name " + s);
-                s = r.Value(i, "Tags");
-                Debug.Log(i + " tags " + s);
-                var inte = (int) r.Value(i, "Ma colonne");
-                Debug.Log(i + " colonne " + inte);
-                
-                s = r.Value(i, "Text");
-                Debug.Log(i + " text " + s);
-                
-                s = r.Value(i, "date");
-                Debug.Log(i + " date " + s);
-                
-                s = r.Value(i, "check");
-                Debug.Log(i + " check " + s);
-                
-                s = r.Value(i, "select");
-                Debug.Log(i + " select " + s);
-                
-                s = r.Value(i, "url");
-                Debug.Log(i + " url " + s);
-                
-                s = r.Value(i, "email");
-                Debug.Log(i + " email " + s);
-                
-                s = r.Value(i, "phone");
-                Debug.Log(i + " phone " + s);
+                var line = r.Lines[i];
+
+                Debug.Log(i + " ------------- ");
+
+                foreach (var cell in line.Cells)
+                {
+                    Debug.Log(cell.Name + " (" + cell.Type + ") =" + cell.Value);
+                }
             }
         }
 
@@ -135,9 +112,78 @@ namespace NotionUnity
         }
     }
 
+    public class TableCell
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public object Value { get; set; }
+
+        public TableCell(JProperty jProperty)
+        {
+            Name = jProperty.Name;
+            Value = ParseLine(jProperty.Value);
+        }
+
+        public object ParseLine(JToken item)
+        {
+            if (item["type"] == null) return null;
+
+            Type = item["type"].ToString();
+            // Debug.Log("[" + row + "] " + columnName + " " + type + "\n" + item);
+            switch (Type)
+            {
+                case "title":
+                    return item["title"].Any() ? item["title"][0]["plain_text"].ToString() : string.Empty;
+
+                case "text":
+                    return item["text"].Any() ? item["text"][0]["text"]["content"].ToString() : string.Empty;
+
+                case "multi_select":
+                    return item["multi_select"].Select(m => m.ToString()).ToArray();
+
+                case "select":
+                    return item["select"]["name"].ToString();
+
+                case "number":
+                    return int.Parse(item["number"].ToString());
+
+                case "date":
+                    return DateTime.Parse(item["date"]["start"].ToString());
+
+                case "checkbox":
+                    return bool.Parse(item["checkbox"].ToString());
+
+                case "people":
+                    return item["people"].Select(m => m.ToString()).ToArray();
+
+                case "url":
+                    return item["url"].ToString();
+
+                case "email":
+                    return item["email"].ToString();
+
+                case "phone_number":
+                    return item["phone_number"].ToString();
+
+                default:
+                    throw new ArgumentException("Unknown/Unsupported item type: " + item["type"]);
+            }
+        }
+    }
+
+    public class TableLine
+    {
+        public TableCell[] Cells { get; set; }
+
+        public TableLine(JToken token)
+        {
+            Cells = token.Select(t => new TableCell((JProperty) t)).ToArray();
+        }
+    }
+
     public class TableResult
     {
-        public JToken[] Lines { get; private set; }
+        public TableLine[] Lines { get; private set; }
 
         public TableResult(JObject json)
         {
@@ -147,9 +193,8 @@ namespace NotionUnity
                 throw new ArgumentException("API Result is not a list");
             }
 
-            var properties = json["results"];
-
-            Lines = properties.Reverse().ToArray();
+            var results = json["results"];
+            Lines = results.Reverse().Select(p => new TableLine(p["properties"])).ToArray();
         }
 
         /// <summary>
@@ -166,60 +211,7 @@ namespace NotionUnity
                 throw new ArgumentException("Line number is greater than line count!");
             }
 
-            try
-            {
-                var item = Lines[row]["properties"][columnName];
-                if (item == null)
-                {
-                    return null;
-                }
-
-                string type = item["type"].ToString();
-                // Debug.Log("[" + row + "] " + columnName + " " + type + "\n" + item);
-                switch (type)
-                {
-                    case "title":
-                        return item["title"].Any() ? item["title"][0]["plain_text"].ToString() : string.Empty;
-
-                    case "text":
-                        return item["text"].Any() ? item["text"][0]["text"]["content"].ToString() : string.Empty;
-
-                    case "multi_select":
-                        return item["multi_select"].Select(m => m.ToString()).ToArray();
-
-                    case "select":
-                        return item["select"]["name"].ToString();
-
-                    case "number":
-                        return int.Parse(item["number"].ToString());
-
-                    case "date":
-                        return DateTime.Parse(item["date"]["start"].ToString());
-
-                    case "checkbox":
-                        return bool.Parse(item["checkbox"].ToString());
-
-                    case "people":
-                        return item["people"].Select(m => m.ToString()).ToArray();
-
-                    case "url":
-                        return item["url"].ToString();
-
-                    case "email":
-                        return item["email"].ToString();
-
-                    case "phone_number":
-                        return item["phone_number"].ToString();
-
-                    default:
-                        throw new ArgumentException("Unknown/Unsupported item type: " + item["type"]);
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.LogException(e);
-                return null;
-            }
+            return Lines[row].Cells.First(l => l.Name == columnName).Value;
         }
     }
 }
